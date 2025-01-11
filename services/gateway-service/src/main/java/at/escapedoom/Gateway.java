a899f90b864915f425ee4eb9afe6966231e71ed9
Megamaxl <maximilian.nowak@outlook.at>
2025-01-11 22:14:38 +0100

    wip but it does smth !

--- services/gateway-service/src/main/java/at/escapedoom/Gateway.java
+++ services/gateway-service/src/main/java/at/escapedoom/Gateway.java
@@ -1,29 +1,43 @@
 package at.escapedoom;
 
+import com.nimbusds.jwt.JWTParser;
+import com.nimbusds.jwt.SignedJWT;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import java.text.ParseException;
+
 @SpringBootApplication
+@EnableWebFluxSecurity
+@RestController
 public class Gateway
 {
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(Gateway.class);
+
     public static void main( String[] args )
     {
         SpringApplication.run(Gateway.class, args);
     }
 
-//    @Bean
-//    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-//        return builder.routes()
-//                .route(p -> p
-//                        .path("/data-api/**")
-//                        .uri("http://host.docker.internal:8090"))
-//                .build();
-//    }
+    @GetMapping(value = "/test")
+    public Mono<String> getHome(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient) throws ParseException {
+        SignedJWT signedJWT = (SignedJWT) JWTParser.parse(authorizedClient.getAccessToken().getTokenValue());
+
+        return Mono.just(authorizedClient.getAccessToken().getTokenValue());
+    }
 }
 
 
