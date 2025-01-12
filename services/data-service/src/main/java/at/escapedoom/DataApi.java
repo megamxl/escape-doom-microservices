b081b764e6e66e39e49779de182ee77edef85f35
Megamaxl <maximilian.nowak@outlook.at>
2025-01-12 14:38:22 +0100

    Now also working with the test service

--- services/data-service/src/main/java/at/escapedoom/DataApi.java
+++ services/data-service/src/main/java/at/escapedoom/DataApi.java
@@ -2,8 +2,10 @@ package at.escapedoom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class DataApi
 {
     public static void main( String[] args )
