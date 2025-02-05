76d6b4236e997fcdcd7785fc16fa1d30d96cc72e
Megamaxl <maximilian.nowak@outlook.at>
2025-02-05 16:22:42 +0100

    added database

---
+++ services/data-service/src/main/java/at/escapedoom/data/TestEntity.java
@@ -0,0 +1,18 @@
+package at.escapedoom.data;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "test_entity")
+public class TestEntity {
+    @Id
+    @GeneratedValue
+    Integer id;
+
+    String name;
+
+
+}
---
+++ services/session-service/src/main/java/at/escapedoom/session/TestEntity.java
@@ -0,0 +1,18 @@
+package at.escapedoom.session;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "test_entity1")
+public class TestEntity {
+    @Id
+    @GeneratedValue
+    Integer id;
+
+    String name;
+
+
+}
