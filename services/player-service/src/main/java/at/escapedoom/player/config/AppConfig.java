c13ee8883542be3ea7da73f5103b9ca6899eed3e
Megamaxl <maximilian.nowak@outlook.at>
2025-02-17 18:26:34 +0100

    communication

--- services/player-service/src/main/java/at/escapedoom/player/config/AppConfig.java
+++ services/player-service/src/main/java/at/escapedoom/player/config/AppConfig.java
@@ -1,7 +1,12 @@
 package at.escapedoom.player.config;
 
+
+import at.escapedoom.spring.communication.session.api.SessionApi;
 import at.escapedoom.player.service.SessionCommunicationService;
 import at.escapedoom.player.service.interfaces.EscapeRoomSessionRepositoryService;
+import at.escapedoom.spring.communication.session.invoker.ApiClient;
+
+import okhttp3.OkHttpClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +22,11 @@ public class AppConfig {
     public EscapeRoomSessionRepositoryService escapeRoomSessionRepositoryService(
             @Autowired SessionCommunicationService escapeRoomSessionRepositoryService) {
         return escapeRoomSessionRepositoryService;
+    }
+
+    @Bean
+    public SessionApi getSessionApi(@Autowired OkHttpClient client) {
+       return new SessionApi( new ApiClient(client).setBasePath("http://localhost:8081/session-api/v1"));
     }
 
 }
