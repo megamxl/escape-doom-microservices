38120be98ce471d9d37b2a123f17920ac3eb7ac2
Thomas Winter <thommy2002@outlook.com>
2025-02-17 20:28:39 +0100

    wip: Working on riddle-api

--- services/data-service/src/main/java/at/escapedoom/data/data/repository/RiddleRepository.java
+++ services/data-service/src/main/java/at/escapedoom/data/data/repository/RiddleRepository.java
@@ -1,11 +1,11 @@
 package at.escapedoom.data.data.repository;
 
-import at.escapedoom.data.data.entity.Scene;
+import at.escapedoom.data.data.entity.Riddle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface ERDataRepository extends JpaRepository<Scene, UUID> {
+public interface RiddleRepository extends JpaRepository<Riddle, UUID> {
 
 
 
--- services/data-service/src/main/java/at/escapedoom/data/services/RiddleService.java
+++ services/data-service/src/main/java/at/escapedoom/data/services/RiddleService.java
@@ -1,9 +1,32 @@
 package at.escapedoom.data.services;
 
+import at.escapedoom.data.data.entity.Riddle;
+import at.escapedoom.data.data.repository.RiddleRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class RiddleService {
 
+    private final RiddleRepository repository;
+
+    public List<Riddle> getAll() {
+        return repository.findAll();
+    }
+
+    public Riddle create(Riddle riddle)  {
+        return repository.save(riddle);
+    }
+
+    public Riddle update(Riddle riddle) {
+        return repository.save(riddle);
+    }
+
+    public void delete(Riddle riddle) {
+        repository.delete(riddle);
+    }
 
 }
