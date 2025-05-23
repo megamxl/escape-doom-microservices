9e6f289e6ab9cebb9e3955e3028cb66fe3574a20
Megamaxl <maximilian.nowak@outlook.at>
2025-05-23 09:57:31 +0200

    Fixed oerder by sorting

--- frontend/app/game-session/leaderboard/[id]/Leaderboard.tsx
+++ frontend/app/game-session/leaderboard/[id]/Leaderboard.tsx
@@ -5,7 +5,7 @@ import {useGetLeaderboard} from "@/app/hooks/leaderboard/useGetLeaderboard";
 import TopThree from "@/app/game-session/leaderboard/[id]/_components/TopThree";
 import LeaderboardRankEntry from "@/app/game-session/leaderboard/[id]/_components/LeaderboardRankEntry";
 import {formatTime, formatTimeInt} from "@/app/utils/formatTime";
-import {getRoomPinHook, useGetRoomPinHook} from "@/app/gen/leaderboard";
+import {getRoomPinHook, useGetRoomPinHook, UserProgress} from "@/app/gen/leaderboard";
 
 const Leaderboard = ({roomPin}: { roomPin: number }) => {
 
@@ -29,15 +29,19 @@ const Leaderboard = ({roomPin}: { roomPin: number }) => {
         return () => clearInterval(interval)
     }, [remainingTime]);
 
+    const sortUsersByScore = (users: UserProgress[]): UserProgress[] =>
+        [...users].sort((a, b) => (b.score ?? 0) - (a.score ?? 0));
+
     return (
+
         <div className={"flex flex-col w-5/6 lg:w-1/2 mt-4 gap-8 justify-center m-auto"}>
             <p className={"text-8xl font-bold self-center"}> {formatTimeInt(remainingTime / 1000)} </p>
 
-            {!isFetching && leaderboardData ? <TopThree topThree={leaderboardData!.slice(0, 3)}/> : "Loading"}
+            {!isFetching && leaderboardData ? <TopThree topThree={sortUsersByScore(leaderboardData)!.slice(0, 3)}/> : "Loading"}
 
             <div className={"flex flex-col gap-3"}>
                 {
-                    !isFetching && leaderboardData ? leaderboardData.slice(3).map((player, idx) => {
+                    !isFetching && leaderboardData ? sortUsersByScore(leaderboardData).slice(3).map((player, idx) => {
                         return (
                             <LeaderboardRankEntry rankingInfo={player} index={idx + 3} key={idx + 3}/>
                         )
