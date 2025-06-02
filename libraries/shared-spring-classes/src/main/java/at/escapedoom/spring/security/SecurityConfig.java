f70cf642b5ff942ee3014c6ffe9ede8220c95b24
Megamaxl <maximilian.nowak@outlook.at>
2025-06-02 18:31:24 +0200

    WIP

--- libraries/shared-spring-classes/src/main/java/at/escapedoom/spring/security/SecurityConfig.java
+++ libraries/shared-spring-classes/src/main/java/at/escapedoom/spring/security/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize ->
                         authorize
+                                .requestMatchers("/actuator/health").permitAll()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())))
                 .csrf(csrf -> csrf.ignoringRequestMatchers("/player-api/gs-guide-websocket/**"));
