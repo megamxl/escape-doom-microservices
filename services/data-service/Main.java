63d84396fe42411be3f50a9b1a9f6f81314cbc12
mopedmaxl <maximilian.nowak@outll>
2025-06-22 09:24:22 +0200

    first deploy

---
+++ services/data-service/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+
+    //region lector-code
+    public static void main(String[] args) {
+        String input = "{{input}}";
+
+        String result = solve(input);
+
+        assert result.equals("{{output}}");
+    }
+    //endregion
+
+    private static void solve(String input) {
+
+    }
+}
